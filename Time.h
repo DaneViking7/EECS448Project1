a317ca7a665d4e301173dc2590cbab00a262c0b4
Aldin Tollison <a983t229@ku.edu>
2018-02-05 18:04:20 -0600

    Doing git tests. Added header comment to Time.h

---
+++ Time.h
@@ -0,0 +1,9 @@
+/*----------------------------------------------------------------------------------------
+File Name: Time.h
+Authors: Natalie LaLuzerne, Annika Kuhnke, Niels Hansen, Aldin Tollison
+Project Number: 01
+Description: This is the header file for the Time file
+Date Due: 02/12/18
+----------------------------------------------------------------------------------------*/
+
+
